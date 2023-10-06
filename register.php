d7671fd3516d0be7912b6edd579d6c8ea88ea254
Venkat8999 <145487374+Venkat8999@users.noreply.github.com>
2023-10-06 16:13:54 -0400

    Add files via upload

---
+++ register.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Welcome to PetConnect</title>
+</head>
+<body>
+    <h1>Registration Form</h1>
+    <form action="register.php" method="post">
+        <input type="text" name="reg_firstname" placeholder="First Name" required><br><br>
+
+        <input type="text" name="reg_lastname" placeholder="Last Name" required><br><br>
+
+        <input type="email" name="reg_email" placeholder="Email" required><br><br>
+
+        <input type="email" name="reg_confirm_email" placeholder="Confirm Email" required><br><br>
+
+        <input type="password" name="reg_password" placeholder="Password" required><br><br>
+
+        <input type="password" name="reg_confirm_password" placeholder="Confirm Password" required><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
